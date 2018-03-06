9a9675202ad6d533065dae81a7b8e267c7304170
michealyan <956462326@qq.com>
2018-03-06 09:02:07 +0800

    complete V1.0.0

--- app/src/main/java/com/michealyan/qiniutestdemo/MainActivity.java
+++ app/src/main/java/com/michealyan/qiniutestdemo/MainActivity.java
@@ -1,11 +1,14 @@
 package com.michealyan.qiniutestdemo;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -15,6 +18,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
@@ -40,23 +44,52 @@ import butterknife.OnClick;
 
 /**
  * 拍照上传主界面
+ *
+ * @author michealyan
+ * @version v1.0.0
  */
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-    private static final int TAKE_PHOTO_REQUEST_CODE = 1;       // 拍摄照片的RequestCode
-    private static final int UPLOAD_PHOTO_REQUEST_CODE = 2;     // 上传图片的RequestCode
-    private static final int APPLY_TAKE_PHOTO_PERMISSION_REQUEST_CODE = 3;     // 申请拍照权限的RequestCode
+    /**
+     * 拍摄照片的RequestCode
+     */
+    private static final int TAKE_PHOTO_REQUEST_CODE = 1;
+    /**
+     * 申请拍照权限的RequestCode
+     */
+    private static final int APPLY_TAKE_PHOTO_PERMISSION_REQUEST_CODE = 2;
+    /**
+     * 更新进度条的Message
+     */
+    private static final int UPLOAD_PROGRESS_MESSAGE = 3;
 
-    private Uri photoStoredUri;         // 拍摄图片存储位置的Uri
+    /**
+     * 拍摄图片存储位置的Uri
+     */
+    private Uri photoStoredUri;
+    private Handler mHandler;
 
+    /**
+     * 拍照图片预览框
+     */
     @BindView(R2.id.iv_photo)
     ImageView photoImg;
+    /**
+     * 拍照按钮
+     */
     @BindView(R2.id.btn_take_photo)
     Button takePhotoBtn;
+    /**
+     * 上传按钮
+     */
     @BindView(R2.id.btn_upload_photo)
     Button uploadPhotoBtn;
-
+    /**
+     * 上传进度条
+     */
+    @BindView(R2.id.pr_upload)
+    ProgressBar uploadProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,24 +97,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+        mHandler = new Handler() {
+
+            @Override
+            public void handleMessage(Message msg) {
+                switch (msg.what) {
+                    case UPLOAD_PROGRESS_MESSAGE:
+                        uploadProgressBar.setProgress(msg.arg1);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        };
     }
 
     @OnClick({R2.id.btn_take_photo, R2.id.btn_upload_photo})
     protected void onclick(View v) {
         switch (v.getId()) {
             case R.id.btn_take_photo:
-                if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                // 动态权限申请
+                if (ContextCompat.checkSelfPermission(this
+                        , Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
                     //权限还没有授予，需要在这里写申请权限的代码
-                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, APPLY_TAKE_PHOTO_PERMISSION_REQUEST_CODE);
+                    ActivityCompat.requestPermissions(this
+                            , new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}
+                            , APPLY_TAKE_PHOTO_PERMISSION_REQUEST_CODE);
                 } else {
                     //权限已经被授予，在这里直接写要执行的相应方法即可
                     takePhoto();
                 }
-
                 break;
             case R.id.btn_upload_photo:
                 if (null != photoStoredUri) {
-
                     uploadPhoto();
                 }
                 break;
@@ -101,9 +149,6 @@ public class MainActivity extends AppCompatActivity {
                     photoStoredUri = null;
                 }
                 break;
-            case UPLOAD_PHOTO_REQUEST_CODE:
-
-                break;
             default:
                 break;
         }
@@ -118,12 +163,15 @@ public class MainActivity extends AppCompatActivity {
      */
     private Uri generatePhotoStoredUri(String prefix, String suffix) {
         String filePath = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + prefix + File.separator;
+        @SuppressLint("SimpleDateFormat")
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
         String fileName = simpleDateFormat.format(new Date()) + suffix;
         File photoFile = new File(filePath);
         // 判断文件的路径是否存在，不存在则创建
         if (!photoFile.exists()) {
-            photoFile.mkdirs();
+            if (!photoFile.mkdirs()) {
+                Toast.makeText(this, "创建储存路径失败", Toast.LENGTH_SHORT).show();
+            }
         }
         photoFile = new File(filePath, fileName);
         Log.d(TAG, filePath + fileName);
@@ -153,30 +201,59 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(takePhotoIntent, TAKE_PHOTO_REQUEST_CODE);
     }
 
+    /**
+     * 调用七牛云sdk上传图片
+     */
     private void uploadPhoto() {
-        Configuration configuration = new Configuration.Builder().zone(AutoZone.autoZone).build();
+        // 显示上传进度对话框
+        uploadProgressBar.setVisibility(View.VISIBLE);
+        Configuration configuration = new Configuration.Builder()
+                // 配置自动选择上传区域
+                .zone(AutoZone.autoZone)
+                .build();
         UploadManager uploadManager = new UploadManager(configuration);
-        String filepath = photoStoredUri.getEncodedPath();          // 要上传的文件路径
-        String key = filepath.substring(filepath.lastIndexOf("/") + 1); // 服务器上文件的唯一标识符，即文件名称
-        String token = Auth.create("YCL7y6ML-tAyhTOE3h2SsdxmkAZ59U4wgVuuHsEr", "IPycePCJe7VlMj4IcjZMhk1r5K3Uqv7E8H8OI9qG").uploadToken("qiniutestdemo");
+        // 要上传的文件路径
+        String filepath = photoStoredUri.getEncodedPath();
+        // 服务器上文件的唯一标识符，即文件名称
+        String key = filepath.substring(filepath.lastIndexOf("/") + 1);
+        // 获取token
+        String token = Auth.create("YCL7y6ML-tAyhTOE3h2SsdxmkAZ59U4wgVuuHsEr"
+                , "IPycePCJe7VlMj4IcjZMhk1r5K3Uqv7E8H8OI9qG")
+                .uploadToken("qiniutestdemo");
+        // 上传结果处理
         UpCompletionHandler upCompletionHandler = new UpCompletionHandler() {
 
             @Override
             public void complete(String key, ResponseInfo info, JSONObject response) {
                 if (info.isOK()) {
-                    Log.i("qiniu", "Upload Success");
+                    // 隐藏上传进度对话框
+                    uploadProgressBar.setVisibility(View.GONE);
+                    photoStoredUri = null;
+                    Glide.with(MainActivity.this).load(R.mipmap.ic_launcher).crossFade().into(photoImg);
+                    // 上传成功，跳转到结果显示页面
+                    Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+                    // 传递图片的URL
+                    intent.putExtra("addr", "http://p54ayw2dx.bkt.clouddn.com/" + key);
+                    startActivity(intent);
                 } else {
-                    Log.i("qiniu", "Upload Fail");
+                    Toast.makeText(MainActivity.this, "上传失败", Toast.LENGTH_SHORT).show();
                 }
             }
         };
+        // 上传进度处理
         UpProgressHandler upProgressHandler = new UpProgressHandler() {
 
             @Override
             public void progress(String key, double percent) {
-                Log.i("qiniu", key + ": " + percent);
+                // 使用handler来更新进度条的进度
+                int progress = (int) (percent * 100);
+                Message message = new Message();
+                message.what = UPLOAD_PROGRESS_MESSAGE;
+                message.arg1 = progress;
+                mHandler.sendMessage(message);
             }
         };
+        // 取消上传处理
         UpCancellationSignal upCancellationSignal = new UpCancellationSignal() {
 
             @Override
@@ -185,6 +262,7 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         UploadOptions uploadOptions = new UploadOptions(null, null, false, upProgressHandler, upCancellationSignal);
+        // 执行上传
         uploadManager.put(filepath, key, token, upCompletionHandler, uploadOptions);
     }
 }
--- app/src/main/java/com/michealyan/qiniutestdemo/ResultActivity.java
+++ app/src/main/java/com/michealyan/qiniutestdemo/ResultActivity.java
@@ -1,13 +1,39 @@
 package com.michealyan.qiniutestdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * 上传结果查看页面
+ *
+ * @author michealyan
+ * @version v1.0.0
+ */
 public class ResultActivity extends AppCompatActivity {
+
+    @BindView(R2.id.iv_photo)
+    ImageView photoImg;             // 图片结果显示框
+    @BindView(R2.id.tv_address)
+    TextView photoAddressText;      // 图片的七牛云存储地址
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        ButterKnife.bind(this);
+
+        // 从intent中获取图片的URL
+        Intent intent = getIntent();
+        String addr = intent.getStringExtra("addr");
+        Glide.with(this).load(addr).crossFade().into(photoImg);
+        photoAddressText.setText("图片地址：" + addr);
     }
 }
